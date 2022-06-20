d1b96031729ab2dbb75afe5b100c8b04d29af2cf
Bruno Caldara <bruno.caldara@cesan.com.br>
2022-06-20 16:21:55 -0300

    Descobre o gato

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,9 +7,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.visit('./src/index.html');
     });
 
-    it('verifica o título da aplicação', () => {
-        const titleApp = 'Central de Atendimento ao Cliente TAT';
-        cy.title().should('eq', titleApp);
+    Cypress._.times(5, () => {
+        it('verifica o título da aplicação', () => {
+            const titleApp = 'Central de Atendimento ao Cliente TAT';
+            cy.title().should('eq', titleApp);
+        });
     });
 
     it('preenche os campos obrigatórios e envia o formulário', () => {
@@ -156,5 +158,45 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.visit('./src/privacy.html');     
 
         cy.contains('Talking About Testing').should('be.visible');
+    });
+
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+    });
+
+    it('preeche a area de texto usando o comando invoke', () => {
+        const longText = Cypress._.repeat('0123456789', 20);
+        
+        cy.get('#open-text-area').invoke('val', longText).should('have.value', longText);
+    });
+
+    it('faz uma requisição HTTP', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should((res) => {
+                const { body, status, statusText } = res;
+
+                expect(status).to.equal(200);
+                expect(statusText).to.equal('OK');
+                expect(body).to.include('CAC TAT');
+            });
+    });
+
+    it.only('encontra o gato escondido', () => {
+        cy.get('#cat').invoke('show').should('be.visible');
+
+        cy.get('#title').invoke('text', 'CAT TAT'); // trocando o texto do H1
     });
 });
